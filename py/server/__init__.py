c669b1c287e63cb9cc09d39bb9f5a06e123b4257
aeonzhang <aeonzhang@tencent.com>
2021-02-09 16:24:20 +0800

    fix

--- py/server/__init__.py
+++ py/server/__init__.py
@@ -1,4 +1,4 @@
 # 此处定义可供外部调用的函数
-FUNC_LIST = {
-    1: echo
+FUNCS = {
+    1: echo.echo
 }
--- py/worker.py
+++ py/worker.py
@@ -1,13 +1,25 @@
 import asyncio
+import server
+from conf.conf import DELI
+
 
 class Worker:
     def __init__(self, inq, outq):
         self.inq = inq
         self.outq = outq
 
-    async def _dispatcher(self):
+    def _dispatcher(self):
         while True:
             if not len(self.inq): continue
-            cur_job = self.inq.popitem(False) # todo collections.OrderedDict
-            
+            fd, job = self.inq.popitem(False) # todo collections.OrderedDict
+            for item in split(job, DELI):
+                if not len(item): continue
+                data = memoryview(item)
+                method_id = data[:2]
+                if FUNCS.get(method_id):
+                    self.outq[fd] = FUNCS[method_id](data[2:])
+                else:
+                    self.outq[fd] = bytearray(-1)
 
+    def run(self):
+        self._dispatcher
