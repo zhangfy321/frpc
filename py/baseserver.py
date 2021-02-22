8da153921edff0083c7428fe139f128fb701c259
aeonzhang <aeonzhang@tencent.com>
2021-02-22 10:34:01 +0000

    fix

--- py/baseserver.py
+++ py/baseserver.py
@@ -1,35 +1,41 @@
 # coding: utf8
 import ioworker
 import worker
-import multiprocessing as mp
+# import multiprocessing as mp
 from conf.conf import *
 from loguru import logger
-import threading
+import threading as tr
 
 
 # Server主要用来加载全局配置和启动多个worker
 class Server:
     def __init__(self):
-        mgr = mp.Manager()
-        self.inq = mgr.dict()
-        self.outq = mgr.dict()
-        self.buffers = mgr.dict()
+        # mgr = mp.Manager()
+        # self.inq = mgr.dict()
+        # self.outq = mgr.dict()
+        # self.buffers = mgr.dict()
+        self.inq = {}
+        self.outq = {}
+        self.buffers = {}
 
     def _set_config(self):
         raise NotImplementedError
 
     def Run(self):
-        IO_NUM = max(round(WORKER_NUM * 0.8), 1)
+        IO_NUM = min(round(WORKER_NUM * 0.8), WORKER_NUM - 1)
         ps = []
         logger.debug(f"_run_ioloop({IO_NUM}) process running...")
+        lock = tr.Lock()
         for _ in range(IO_NUM):
-            p = mp.Process(target=Server._run_ioloop, args=(self.inq, self.outq, self.buffers))
+            # p = mp.Process(target=Server._run_ioloop, args=(self.inq, self.outq, self.buffers))
+            p = tr.Thread(target=Server._run_ioloop, args=(self.inq, self.outq, self.buffers, lock))
             p.daemon = True
             ps.append(p)
 
         logger.debug(f"_run_handler({WORKER_NUM - IO_NUM}) process running...")
         for _ in range(WORKER_NUM - IO_NUM):
-            p = mp.Process(target=Server._run_handler, args=(self.inq, self.outq))
+            # p = mp.Process(target=Server._run_handler, args=(self.inq, self.outq))
+            p = tr.Thread(target=Server._run_handler, args=(self.inq, self.outq))
             p.daemon = True
             ps.append(p)
 
@@ -37,13 +43,13 @@ class Server:
         for p in ps:
             p.start()
 
-        # 阻塞父进程
+        # # 阻塞父线程
         for p in ps:
             p.join()
 
     @staticmethod
-    def _run_ioloop(inq, outq, buffers):
-        ioworker.IOWorker(inq, outq, buffers).run()
+    def _run_ioloop(inq, outq, buffers, lock):
+        ioworker.IOWorker(inq, outq, buffers, lock).run()
 
     @staticmethod
     def _run_handler(inq, outq):
--- py/ioworker.py
+++ py/ioworker.py
@@ -4,20 +4,20 @@ import utils.base as base
 from conf.conf import *
 from queue import Queue
 import socket
-
+import threading
 
 class IOWorker: 
-    def __init__(self, inq, outq, buffers):
+    def __init__(self, inq, outq, buffers, lock):
         logger.debug("init server socket")
         self.m_sock = base.init_server_socket()
         self.epoll = select.epoll()
         self.m_sock.setsockopt(socket.IPPROTO_TCP, socket.TCP_NODELAY, 1)
         self.epoll.register(self.m_sock.fileno(), select.EPOLLIN | select.EPOLLET)
-
         self.conns = {} 
         self.inq = inq  # 入包
         self.outq = outq  # 出包
         self.buffers = buffers  # 用于处理粘包
+        self.lock = lock
 
     def run(self):
         while True:
@@ -25,12 +25,13 @@ class IOWorker:
             for fd, ev in epoll_lst:
                 if fd == self.m_sock.fileno():
                     self.on_connect()
-                elif ev & select.EPOLLIN:
-                    self.on_read(fd)
                 elif ev & select.EPOLLOUT:
                     self.on_write(fd)
+                elif ev & select.EPOLLIN:
+                    self.on_read(fd)
                 else:
                     self.on_close(fd)
+
 
     def on_connect(self):
         try:
@@ -39,72 +40,60 @@ class IOWorker:
                 conn.setblocking(0)
                 self.epoll.register(conn.fileno(), select.EPOLLIN | select.EPOLLET)
                 self.conns[conn.fileno()] = conn
-                self.inq[conn.fileno()] = bytearray()
-                self.outq[conn.fileno()] = bytearray()
-                self.buffers[conn.fileno()] = bytearray()
-        except Exception as e:
-            # logger.debug(e)
+
+                with self.lock:
+                    self.inq[conn.fileno()] = bytearray()
+                    self.outq[conn.fileno()] = bytearray()
+                    self.buffers[conn.fileno()] = bytearray()
+        except socket.error:
             pass
 
     def on_read(self, fd):
         conn = self.conns[fd]
         data = bytearray()
-        # logger.debug(f"EPOLL IN: {fd}")
         try:
             while True:
                 data += conn.recv(1024)
-        except Exception as e:
-            # logger.debug(e)
+        except socket.error:
             pass
-        # logger.debug(f"recv data legth: {len(data)}")
-        msgs = data.split(DELIM)  # 每个消息尾部加上DELIM
-        if len(msgs) == 0:
+
+        msgs = data.split(DELIM)
+        if len(msgs) == 0: # 当前包不构成完整消息，与buffer拼接
             self.buffers[fd] = self.buffers[fd] + bytearray(data)
         else:
             for idx, msg in enumerate(msgs):
                 if len(msg):
-                    last = self.buffers.get(fd)
+                    last = self.buffers.get(fd, bytearray())
                     if idx < len(msgs):  # 后面还有说明本段构成了完整消息
                         self.inq[fd] = last + bytearray(msg) + DELIM
-                        # logger.debug("put to inq...")
                         self.buffers[fd] = bytearray()
                     else: 
                         self.buffers[fd] = bytearray(msg)
-            # while not len(self.inq[fd]):
-            #     logger.debug(f"message: {self.inq[fd].get()}")
-        # logger.debug(f"入包数据: {data}")
         self.epoll.modify(fd, select.EPOLLOUT | select.EPOLLET)
  
     def on_write(self, fd):
-        # logger.debug("EPOLL OUT")
-        if not len(self.outq[fd]):  # 没有回包，下次再说
-            self.epoll.modify(fd, select.EPOLLOUT | select.EPOLLET)  # 手动触发out事件
-            return
-        logger.debug(f"回包数据: {self.outq[fd]}")
         data = memoryview(self.outq[fd])
+        if not len(self.outq[fd]):
+            logger.error("出包为空")
+            self.epoll.modify(fd, select.EPOLLOUT | select.EPOLLET)
         try:
-            while len(data):
+            while len(data) > 0:
                 cnt = self.conns[fd].send(data)
                 data = data[cnt:]
-        except Exception as e:
-            # logger.error(e)
-            if len(data): return  # 缓冲不够 没有写完 等下次OUT事件再写
-        if not len(data):
-            self.epoll.modify(fd, select.EPOLLET)
-            self.conns[fd].shutdown(socket.SHUT_RDWR)  # 如何处理对端关闭？ 
+        except socket.error:
+            pass
+        # self.conns[fd].sendall(self.outq[fd])
+        # self.epoll.modify(fd, select.EPOLLIN | select.EPOLLET)
+        self.on_close(fd)
 
-    def on_close(self, fd):
-        # logger.debug("CLOSE OR ERROR")   # todo 超时控制 用堆？链表？map？
+    def on_close(self, fd): # todo 超时控制 用堆？链表？map？
+        self.conns[fd].shutdown(socket.SHUT_RDWR)
         self.epoll.unregister(fd)
         self.conns[fd].close()
         del self.conns[fd]
-        if self.inq.get(fd, None):
-            del self.inq[fd]
-        del self.outq[fd]
-
-# class Sock: # 计时/排序
-#     def __init__(self, fd):
-#         self.fd = fd
-#         self.time = time.time()
-#     def __lt__(self, other):
-#         return self.time < other.time
+        with self.lock:
+            try:
+                del self.outq[fd]
+                del self.buffers[fd]
+            except:
+                pass
